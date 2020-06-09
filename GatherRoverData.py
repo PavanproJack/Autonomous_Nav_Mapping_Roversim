a38bdcfc5e4dd5e5bdb720048a8877bb515cec2e
PavanKumar Kavvuri <PavanproJack@github.com>
2020-06-09 08:52:02 +0100

    debugging with rover data

---
+++ GatherRoverData.py
@@ -0,0 +1,112 @@
+import matplotlib.image as mpimage
+import matplotlib.pyplot as plt
+import numpy as np
+
+import socketio
+import eventlet
+import eventlet.wsgi
+import time
+from flask import Flask
+from supporting_functions import update_rover
+from birdEyeView import perspectiveTransform
+
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+
+ground_truth = mpimage.imread('./calibration_images/map_bw.png')
+
+ground_truth_3d = np.dstack((ground_truth*0, ground_truth*255, ground_truth*0)).astype(np.float)
+
+
+# Define RoverState() class to retain rover state parameters
+class RoverState():
+    def __init__(self):
+        self.start_time = None # To record the start time of navigation
+        self.total_time = None # To record total duration of naviagation
+        self.img = None # Current camera image
+        self.pos = None # Current position (x, y)
+        self.yaw = None # Current yaw angle
+        self.pitch = None # Current pitch angle
+        self.roll = None # Current roll angle
+        self.vel = None # Current velocity
+        self.steer = 0 # Current steering angle
+        self.throttle = 0 # Current throttle value
+        self.brake = 0 # Current brake value
+        self.nav_angles = None # Angles of navigable terrain pixels
+        self.nav_dists = None # Distances of navigable terrain pixels
+        self.ground_truth = ground_truth_3d # Ground truth worldmap
+        self.mode = 'forward' # Current mode (can be forward or stop)
+        self.throttle_set = 0.2 # Throttle setting when accelerating
+        self.brake_set = 10 # Brake setting when braking
+        self.scale = 10
+        # The stop_forward and go_forward fields below represent total count
+        # of navigable terrain pixels.  This is a very crude form of knowing
+        # when you can keep going and when you should stop.  Feel free to
+        # get creative in adding new fields or modifying these!
+        self.stop_forward = 50 # Threshold to initiate stopping
+        self.go_forward = 500 # Threshold to go forward again
+        self.max_vel = 2 # Maximum velocity (meters/second)
+        # Image output from perception step
+        # Update this image to display your intermediate analysis steps
+        # on screen in autonomous mode
+        self.vision_image = np.zeros((160, 320, 3), dtype=np.float) 
+        # Worldmap
+        # Update this image with the positions of navigable terrain
+        # obstacles and rock samples
+        self.worldmap = np.zeros((200, 200, 3), dtype=np.float) 
+        self.samples_pos = None # To store the actual sample positions
+        self.samples_to_find = 0 # To store the initial count of samples
+        self.samples_located = 0 # To store number of samples located on map
+        self.samples_collected = 0 # To count the number of samples collected
+        self.near_sample = 0 # Will be set to telemetry value data["near_sample"]
+        self.picking_up = 0 # Will be set to telemetry value data["picking_up"]
+        self.send_pickup = False # Set to True to trigger rock pickup
+# Initialize our rover 
+Rover = RoverState()
+
+
+
+frame_counter = 0
+# Initalize second counter
+second_counter = time.time()
+fps = None
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+        global Rover
+        # Initialize / update Rover with current telemetry
+        Rover, image = update_rover(Rover, data)
+        image = perspectiveTransform(Rover)
+        commands = (Rover.throttle, Rover.brake, Rover.steer)
+        send_control(commands, '', '')
+
+
+def send_control(commands, image_string1, image_string2):
+    # Define commands to be sent to the rover
+    data={
+        'throttle': commands[0].__str__(),
+        'brake': commands[1].__str__(),
+        'steering_angle': commands[2].__str__(),
+        'inset_image1': image_string1,
+        'inset_image2': image_string2,
+        }
+    # Send commands via socketIO server
+    sio.emit(
+        "data",
+        data,
+        skip_sid=True)
+    eventlet.sleep(0)   
+    
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    
+
+if __name__ == '__main__':
+    app = socketio.Middleware(sio, app)
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    
